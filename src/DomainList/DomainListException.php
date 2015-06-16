15554156e908b279980feeb39220f08d0d5ca4e6
Tim Groeneveld <tim@timg.ws>
2015-06-16 12:06:39 +1000

    DomainListException

---
+++ src/DomainList/DomainListException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace timgws;
+
+/**
+ * Class DomainListException
+ *
+ * A list of domain names, automatically adds the TLD to the domain.
+ */
+class DomainListException extends \Exception
+{
+
+}
